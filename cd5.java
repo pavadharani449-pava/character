a78f0eefebe43fb075af764150bdbf57bf90f8e2
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-17 09:16:45 +0530

    Add files via upload

---
+++ cd5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class cd5 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        char b=sc.next().charAt(0);
+        if(a==b)
+        {
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("Not");
+        }
+    }
+        
+    }
