7dc23dbfd733f73e43b0e3c1dd514be856643adc
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-17 09:16:28 +0530

    Add files via upload

---
+++ cd4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class cd4 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        if(((int)a>=65 && (int)a<=90)||((int)a>=97 && (int)a<=122))
+        {
+            System.out.println("Alphabet");
+        }
+        else{
+            System.out.println("Not");
+        }
+    }
+    
+}
